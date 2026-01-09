e27ea490c20391b2d22ab2f0b627ec6713cdf1a7
salaga999999999 <salaga999999999@gmail.com>
2026-01-09 18:41:03 +0700

    Task one complete

---
+++ ConsoleApp/EnumerableExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public static class EnumerableExtension
+    {
+        public static T GetMax<T>(this IEnumerable collection, Func<T, float> convertToNumber) where T : class
+        {
+            if (collection == null) return null!;
+
+            T MaxElement = null!;
+            float MaxValue = float.MinValue;
+
+            foreach (var item in collection)
+            {
+                T? element = item as T;
+
+                if (element != null)
+                {
+                    float currentValue = convertToNumber(element);
+
+                    if (currentValue > MaxValue)
+                    {
+                        MaxValue = currentValue;
+                        MaxElement = element;
+                    }
+                }
+            }
+
+            return MaxElement!;
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var strings = new List<string> { "Word1", "Hellp", "porgramm" };
+            Console.WriteLine($"Longest string: {strings.GetMax<string>(s => s.Length)}");
+
+            var products = new[]
+            {
+                new { Name = "Product1", Price = 10.0 },
+                new { Name = "Product3", Price = 20.0 },
+                new { Name = "Product2", Price = 120.0 }
+            };
+            var maxPrice = products.GetMax<object>(p => { dynamic d = p; return (float)d.Price; });
+            Console.WriteLine($"Most expancive product {maxPrice}");
+
+
+
+            Console.WriteLine("Program Complete.");
         }
     }
 }
